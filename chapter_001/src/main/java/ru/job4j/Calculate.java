c4422df8c997c879479abc22fdf14bfeb467a04a
ShaevAndrey <spinoza33@yandex.ru>
2017-11-01 22:20:30 +0300

    1.3.JavaDoc

---
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -0,0 +1,20 @@
+package ru.job4j;
+
+/**
+ * Class Calculate предназначен для выполнения задания 1.3.JavaDoc
+ * @author Shaev Andrey
+ * @version 1.01
+ * @since 01.11.17
+*/
+
+public class Calculate{
+	
+/** 
+ *метод main служит для вывода на экран "Hello world"
+*/
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world");	
+	
+    }
+}
